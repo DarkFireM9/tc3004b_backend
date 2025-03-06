d32af6bcc6f30f1c410b871ce35beccb857e6c44
Aaron Hernandez <darkfire2369@outlook.com>
2025-03-06 12:58:16 -0600

    Backend actualizado

--- utils/sql.js
+++ utils/sql.js
@@ -1,13 +1,13 @@
-import sql from 'mssql'
+import sql from "mssql";
 
 const sqlConfig = {
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PWD,
   database: process.env.DB_NAME,
   server: process.env.DB_SERVER,
   options: {
     encrypt: true, // for azure
-    trustServerCertificate: true // change to true for local dev / self-signed certs
+    trustServerCertificate: true,// change to true for local dev / self-signed certs
   },
 };
 
