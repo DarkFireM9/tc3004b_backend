3112c03b281c81d7ba8e08c7ef6c1d89219ae2eb
DarkFireM9 <darkfire2369@outlook.com>
2025-03-31 23:56:16 -0600

    Hash

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ import morgan from "morgan";
 import indexRoutes from "./routes/index.routes.js";
 import itemsRoutes from "./routes/items.routes.js";
 import items2Routes from "./routes/items2.routes.js";
+import items3Routes from "./routes/items3.routes.js";
 import loginRoutes from "./routes/login.routes.js";
-import { connectDB } from "./utils/mogodb.js";
+// import { connectDB } from "./utils/mogodb.js";
 
 const app = express();
 
-connectDB();
+//connectDB();
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -19,7 +20,8 @@ app.use(express.json());
 
 app.use(indexRoutes);
 app.use(itemsRoutes);
-app.use(items2Routes);
+//app.use(items2Routes);
+app.use(items3Routes);
 app.use(loginRoutes);
 
 app.listen(5001, console.log("http://localhost:5001"));
